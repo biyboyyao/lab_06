27a621101a8c35f9bd1733e718de02e31810ed03
Yaotian Zhang <y.zhang3@ufl.edu>
2023-03-09 15:08:01 -0500

    Walter White

---
+++ lab06.py
@@ -0,0 +1,9 @@
+def encoder(password):
+    value = str()
+    for i in password:
+        num = int(i) + 3
+        if num >= 10:
+            num = num - 10
+        value += str(num)
+    return value
+
